97fb1e85c7b4f8223c5e5cb6eb0885209cbe1de3
MohamedRach <rachidi.mohamed.sadek@gmail.com>
2023-06-03 03:06:49 +0100

    graphql server

---
+++ app.js
@@ -0,0 +1,20 @@
+require('dotenv').config();
+const express = require('express');
+const {graphqlHTTP} = require('express-graphql');
+const schema = require('./schema/schema');
+const mongoose = require('mongoose');
+const app = express();
+const dbURI = process.env.DB_URL;
+
+app.use('/graphql', graphqlHTTP({
+    schema,
+    graphiql:true
+}));
+mongoose.connect(dbURI)
+    .then((result) => {
+        console.log("success");
+        app.listen(4000, () => {
+            console.log('now listening on port 4000');
+        })
+    })
+    .catch((err) => console.log(err));
